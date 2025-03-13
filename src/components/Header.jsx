8500f2975f84d9f70e23507ccda2104afabd6516
Alejandro Valera <ale24vv@gmail.com>
2025-03-13 11:36:36 -0400

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,6 @@
-import { useMemo } from "react";
 
-export default function Header({ cart, removeFromCart, decreaseQuantity, increaseQuantity, clearCart }) {
-  
-  // State Derivado
-  const isEmpty = useMemo( () => cart.length === 0, [cart])
-  const cartTotal = useMemo(() => cart.reduce((total, item) => total + item.quantity * item.price, 0), [cart])
+
+export default function Header({ cart, removeFromCart, decreaseQuantity, increaseQuantity, clearCart, isEmpty, cartTotal }) {
 
 
   return (
